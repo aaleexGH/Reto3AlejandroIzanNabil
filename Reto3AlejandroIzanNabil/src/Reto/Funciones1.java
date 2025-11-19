f92746ef722ba348665826f572fb11616aa7518f
aaleexGH <1DAM@A11-PC17>
2025-11-19 13:49:54 +0100

    hola

--- Reto3AlejandroIzanNabil/src/Reto/Funciones1.java
+++ Reto3AlejandroIzanNabil/src/Reto/Funciones1.java
@@ -1,5 +1,52 @@
 package Reto;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Funciones1 {
+
+	public static String capitalizarString(String c) {
+		return c.substring(0, 1).toUpperCase() + c.substring(1).toLowerCase();
+	}
+
+	public static String menu1(Scanner sc) {
+		String palabra = "";
+		String corta = "";
+		String primera = "";
+		boolean primerapalabra = true;
+
+		do {
+			System.out.println("Dime un nombre: ");
+			String n = sc.nextLine();
+
+			String nombrebien = capitalizarString(n);
+			System.out.println("Nombre capitalizado: " + nombrebien);
+
+			String apodo = generarApodo(n);
+			System.out.println("Apodo: " + apodo);
+
+			if (n.length() < corta.length()) {
+				corta = n;
+			}
+
+			if (n.compareTo(primera) < 0) {
+				primera = n;
+			}
+
+		} while (palabra.length() > 0);
+
+		if (!primerapalabra) {
+			System.out.println("Nombre más corto: " + capitalizarString(corta));
+			System.out.println("Nombre alfabéticamente primero: " + capitalizarString(primera));
+		}
+		return "";
+	}
+
+	public static String generarApodo(String a) {
+		if (a.length() > 4) {
+			return a.substring(0, 3) + a.lastIndexOf(a) + a.length();
+		}
+		return a + a.length();
+	}
 
 }
--- Reto3AlejandroIzanNabil/src/Reto/Reto3.java
+++ Reto3AlejandroIzanNabil/src/Reto/Reto3.java
@@ -1,10 +1,48 @@
 package Reto;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import Reto.Funciones1;
+import Reto.Funciones2;
+import Reto.Funciones3;
+
 public class Reto3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in).useLocale(Locale.US);
 
+		int opcion = 0; 
+		do {
+			System.out.println("¿QUE DESEAS HACER?: ");
+			System.out.println("1. Nombres");
+			System.out.println("2. Palabras");
+			System.out.println("3. Frases");
+			System.out.println("0. Salir");
+			
+			String op = sc.nextLine();
+			opcion = Integer.parseInt(op);
+			
+			switch (opcion) {
+			case 1:
+				Funciones1.menu1(sc);
+				break;
+			case 2:
+				Funciones1.menu1(sc);
+				break;
+			case 3:
+				Funciones1.menu1(sc);
+				break;
+			case 0:
+				System.out.println("Saliendo...");
+				break;
+
+			default:
+				break;
+			}
+		} while (opcion != 0);
+		
+		
 	}
 
 }
